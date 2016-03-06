a565e1a33fbc8cda832817e4f9599dcebddab41e
sawangpong muadphet <sawangpongm@gmail.com>
2016-03-06 18:36:46 +0700

    commit

--- docs/build/html/searchindex.js
+++ docs/build/html/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to Linux Book&#8217;s documentation!"],titleterms:{book:0,document:0,indic:0,linux:0,tabl:0,welcom:0}})
+Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Linux Book&#8217;s documentation!"],titleterms:{book:0,document:0,indic:0,linux:0,tabl:0,welcom:[]}})
